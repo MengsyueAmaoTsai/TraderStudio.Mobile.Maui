702ab49785df38730c2758317fd67e926fd71c73
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-11-01 13:55:03 +0800

    Install maui check tool

--- Src/AppShell.xaml.cs
+++ Src/AppShell.xaml.cs
@@ -1,9 +1,17 @@
-﻿namespace RichillCapital.TraderStudio.Mobile;
+﻿using RichillCapital.TraderStudio.Mobile.Pages;
+
+namespace RichillCapital.TraderStudio.Mobile;
 
 public sealed partial class AppShell : Shell
 {
     public AppShell()
     {
         InitializeComponent();
+        RegisterRoutes();
+    }
+
+    private static void RegisterRoutes()
+    {
+        Routing.RegisterRoute(nameof(SignalSourcesPage), typeof(SignalSourcesPage));
     }
 }
---
+++ Src/Pages/SignalSourcesPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace RichillCapital.TraderStudio.Mobile.Pages;
+
+public sealed partial class SignalSourcesPage : ContentPage
+{
+    public SignalSourcesPage()
+    {
+        InitializeComponent();
+    }
+}
